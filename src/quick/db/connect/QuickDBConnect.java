7d199bd2cd7b6b318b8ac613f3d8b44f0c7d95ae
JordanF <>
2015-07-09 13:29:43 -0400

    initial commit

---
+++ src/quick/db/connect/QuickDBConnect.java
@@ -0,0 +1,10 @@
+package quick.db.connect;
+
+/**
+ * This class will create a Connection without the user
+ * needing to specify the driver and URL
+ */
+public class QuickDBConnect {
+    
+    
+}
